c874bffed6506bc18456ed633573a170c1d47681
tommydemarco <tommaso.demarco95@gmail.com>
2020-12-30 00:26:38 +0100

    ToastMessage component styled, header styled

--- src/components/ToastMessage/ToastMessage.jsx
+++ src/components/ToastMessage/ToastMessage.jsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import "./ToastMessage.scss"
 
 const ToastMessage = ({ title, message, type, icon, additionalClass }) => {
   const classes = ["toast"]
+
+  const [active, setActive] = useState(false)
+
+  if (active) {
+    classes.push("active")
+  }
 
   if (typeof additionalClass === "string" && additionalClass.length) {
     classes.push(additionalClass)
@@ -13,12 +19,24 @@ const ToastMessage = ({ title, message, type, icon, additionalClass }) => {
     classes.push("toast--" + type)
   }
 
+  useEffect(() => {
+    setTimeout(() => setActive(true), 2000)
+  }, [])
+
   return (
     <div className={classes.join(" ")}>
-      <i className={"fa " + icon}></i>
+      <span
+        className="toast__x"
+        onClick={() => {
+          setActive(false)
+        }}
+      >
+        &#10005;
+      </span>
+      <i className={"fa " + icon + " toast__icon"}></i>
       <div className="toast__text">
-        <span>{title}</span>
-        <span>{message}</span>
+        <span className="toast__title">{title}</span>
+        <span className="toast__message">{message}</span>
       </div>
     </div>
   )
